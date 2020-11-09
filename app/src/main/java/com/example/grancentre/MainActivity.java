f3f7d52aaa037e943c901e7b3e769e9805b826b1
Aleix Serra <aleixserrarecolons01@gmail.com>
2020-11-09 09:24:37 +0100

    Creació Intents

--- app/src/main/java/com/example/grancentre/MainActivity.java
+++ app/src/main/java/com/example/grancentre/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.grancentre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] buttonIDs = new int[] {R.id.bRestarurant, R.id.bHotel, R.id.bCartellera, R.id.bParking };
+        for (int buttonID : buttonIDs) {
+            ImageButton b = (ImageButton) findViewById(buttonID);
+            b.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button b = (Button)v;
+        if(b.getId() == R.id.bRestarurant) {
+            Intent intent = new Intent(this, ActivityRestaurant.class);
+            startActivity(intent);
+        }
+        else if(b.getId() == R.id.bHotel) {
+            Intent intent = new Intent(this, ActivityHotel.class);
+            startActivity(intent);
+        }
+        else if(b.getId() == R.id.bCartellera) {
+            Intent intent = new Intent(this, ActivityCartellera.class);
+            startActivity(intent);
+        }
+        else {
+            Intent intent = new Intent(this, ActivityParking.class);
+            startActivity(intent);
+        }
     }
 }
