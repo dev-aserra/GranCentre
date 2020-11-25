6dad49b1f795a0630533b6102f29d6b6247aab67
Guillem <eugir33@gmail.com>
2020-11-25 09:35:26 +0100

    Eliminació de codi no necessari.

--- app/src/main/java/com/example/grancentre/ActivityHotel.java
+++ app/src/main/java/com/example/grancentre/ActivityHotel.java
@@ -22,6 +22,7 @@ import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 public class ActivityHotel extends AppCompatActivity implements Hotel2.OnHotel2FragmentListener, Hotel2.ViewInterface {
+
     private ViewPager viewPager;
     private TabLayout tabLayout;
     private TabItem hotel2,hotel3,hotel4;
@@ -29,6 +30,7 @@ public class ActivityHotel extends AppCompatActivity implements Hotel2.OnHotel2F
     public PagerAdapter PageAdapter;
     FragmentPagerAdapter fragmentPagerAdapter;
     Fragment fragment2 = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
@@ -44,15 +46,8 @@ public class ActivityHotel extends AppCompatActivity implements Hotel2.OnHotel2F
         PageAdapter = new PageAdapter(frm,tabLayout.getTabCount());
         viewPager.setAdapter(PageAdapter);
         FragmentPagerAdapter fragmentPagerAdapter = (FragmentPagerAdapter) viewPager.getAdapter();
-        //  for (int i =0; i< fragmentPagerAdapter.getCount(); i++)
-        fragment2 = (Fragment) fragmentPagerAdapter.instantiateItem(viewPager,viewPager.getCurrentItem());/*
-            if (fragment2!= null && fragment2.getView()!= null) {
-               // Fragment fragment = frm.findFragmentById(R.id.hotel2);
-               // if (fragment != null) {
-                com.google.android.material.button.MaterialButton boto1 = fragment2.getView().findViewById(R.id.website);
-                boto1.setOnClickListener(this);
-              //  }
-            }*/
+        fragment2 = (Fragment) fragmentPagerAdapter.instantiateItem(viewPager,viewPager.getCurrentItem());
+
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -83,16 +78,13 @@ public class ActivityHotel extends AppCompatActivity implements Hotel2.OnHotel2F
         //TODO: Ja han clicat al botó del fragment, ara què cal fer?
         switch (v.getId()){
             case R.id.websiteHotel2Estrellas_1:
-                //Intent intent2 = new Intent(Intent.ACTION_DIAL, Uri.parse((String) resultat.getText()));
                 viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://www.hotel-bb.com/es/hotel/barcelona-granollers"));
                 startActivity(viewIntent);
                 break;
             case R.id.telefonHotel2Estrellas_1:
-                //Intent intent2 = new Intent(Intent.ACTION_DIAL, Uri.parse((String) resultat.getText()));
-                Intent intent2 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:tel:938407132"));
-                startActivity(intent2);
+                viewIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:tel:938407132"));
+                startActivity(viewIntent);
                 break;
-            //resultat.setText("Hola"); break;
         }
     }
 }
